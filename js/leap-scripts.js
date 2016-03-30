7d5e56562abf031c1caf37cc31b66ea82c42041c
Amber Baird and Anthony Gold <amber@>
2016-03-30 09:38:51 -0700

    add alert for blank

--- js/leap-scripts.js
+++ js/leap-scripts.js
@@ -1,6 +1,7 @@
 // business logic
 var leapYear = function(year) {
-  if (year < 0) {
+  var blank = "";
+  if ((blank === "") || (year < 0)) {
     alert("please enter a year");
   } else if
     ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
